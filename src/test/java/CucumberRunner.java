381f3b16047a7fc295035010e77ca6879adfcc10
Iurii Varankin <varan555@yandex.ru>
2020-06-12 20:28:54 +0300

    InitCommit

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         //, "json:target/allure-results/" {"pretty"}
-        plugin = {"pretty"},
+        plugin = {"pretty", "json:target/allure-results/results.json"},
         glue = {"steps"},
         features = {"src/test/resources/"}
         )
